9a8d7e6253aea2368c4c8113673d9fd41c51661a
reshmanayak-1999 <reshmanayak1999@gmail.com>
2024-07-22 22:03:10 +0530

    added testng.xml file and allure reports

---
+++ src/test/java/base/AllureReport.java
@@ -0,0 +1,16 @@
+package base;
+
+
+
+
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class AllureReport {
+
+    @Test
+    public void test1()  {
+        System.out.println("hello world");
+    }
+}
